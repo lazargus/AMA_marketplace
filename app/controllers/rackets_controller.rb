5add6b6047e3422b983c55834854be3ba6b91c87
Alexandre Deniau <deniaualexandre@gmail.com>
2020-05-19 11:25:58 +0200

    pull le code avant d'upload destroy update

--- app/controllers/rackets_controller.rb
+++ app/controllers/rackets_controller.rb
@@ -1,12 +1,12 @@
 class RacketsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_racket, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @rackets = Racket.all
   end
 
   def show
-    @racket = Racket.find(params[:id])
   end
 
   def new
@@ -14,6 +14,33 @@ class RacketsController < ApplicationController
   end
 
   def create
-
   end
+
+  def edit
+  end
+
+  def update
+    if @racket.update(racket_params)
+      redirect_to racket_path(@racket), notice: 'Racket was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+      @racket = Racket.find(params[:id])
+      @racket.destroy
+      redirect_to rackets_path, notice: 'Racket was successfully destroyed.'
+  end
+
+  private
+
+  def set_racket
+      @racket = Racket.find(params[:id])
+  end
+
+  def racket_params
+    params.require(:racket).permit(:description, :model, :year, :price, :location)
+  end
+
 end

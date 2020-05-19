116398a1aedf3d909c1658864ed505a2c98816e5
benoit barges <benoitbarges@gmail.com>
2020-05-19 15:44:15 +0200

    bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,6 +17,12 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
   private
 
   def booking_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: :index
+  resources :bookings, only: [:index, :destroy]
 end

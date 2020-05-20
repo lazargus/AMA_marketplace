63e2d87fce95c9c16f1f2ed0a44338bb360fdca1
Francois <francois.desazars@gmail.com>
2020-05-20 12:49:04 +0200

    finished map integration on racket show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_093511) do
+ActiveRecord::Schema.define(version: 2020_05_20_084655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_05_19_093511) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.boolean "availability", default: true
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_rackets_on_user_id"
   end
 

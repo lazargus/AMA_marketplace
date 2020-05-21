26bd513fbc3e180141e9a851a45f7b3d631cbe1b
Francois <francois.desazars@gmail.com>
2020-05-21 10:40:18 +0200

    touches on the form

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,11 @@
 import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-flatpickr("#range_start", {
+
+flatpickr("#start_date", {
   altInput: true,
-  plugins: [new rangePlugin({ input: "#range_end"})]
+  plugins: [new rangePlugin({ input: "#end_date"})]
 });
+
 
 flatpickr(".datepicker", {});

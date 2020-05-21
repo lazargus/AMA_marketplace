699d5ab02372effaf63c62880be43f3a965165b0
benoit barges <benoitbarges@gmail.com>
2020-05-21 12:27:24 +0200

    booking pundi done

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,23 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end

ebd73bb8d7f9646b9b618a91f3058e51a505939b
Augustin Lazarus <augustin.lazarus@free.fr>
2020-05-22 13:07:48 +0200

    many problems resolved

--- app/models/racket.rb
+++ app/models/racket.rb
@@ -1,6 +1,6 @@
 class Racket < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   belongs_to :user
   has_one_attached :photo
 
